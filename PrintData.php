c363dad8c1958ba259cf7ccc310017d32ba9ec70
bagusp <bagusp0518@gmail.com>
2023-01-17 19:18:33 +0700

    init print

---
+++ PrintData.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once 'Helpers/ReceiptPrinter/ReceiptPrinter.php';
+
+class PrintData
+{
+    public $dotenv;
+
+    public function __construct()
+    {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+    }
+
+    public function get_data()
+    {
+        $response = array(
+            'status' => true,
+            'message' => 'Success Get Data.',
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function post_data()
+    {
+        $request = json_decode(file_get_contents('php://input'), true);
+
+        $this->print($request);
+
+        $response = array(
+            'status' => true,
+            'message' => 'Success Post Data.',
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    function print($request) {
+        $store_name = $request['store_name'];
+        $store_address = $request['store_address'];
+        $store_phone = $request['store_phone'];
+        $store_email = $request['store_email'];
+        $store_website = $request['store_website'];
+        $buyer_name = $request['buyer_name'];
+        $currency = $request['currency'];
+        $tax_percentage = $request['tax_percentage'];
+        $image_path = $request['image_path'];
+        $amount_paid = $request['amount_paid'];
+        $total_price = $request['total_price'];
+        $items = $request['items'];
+
+        // Init printer
+        $printer = new ReceiptPrinter;
+        $printer->init(
+            $_ENV['PRINTER_CONNECTOR_TYPE'],
+            $_ENV['PRINTER_CONNECTOR_DESCRIPTOR'],
+        );
+
+        // Set store info
+        $printer->setStore($store_name, $store_address, $store_phone, $store_email, $store_website);
+        $printer->setBuyer($buyer_name);
+        $printer->setCurrency($currency);
+        $printer->setRequestAmount($total_price);
+        $printer->setAmountPaid($amount_paid);
+        $printer->setTax($tax_percentage);
+
+        // Add items
+        foreach ($items as $detail) {
+            $printer->addItem(
+                $detail['name'] . ' - ' . $detail['unit'],
+                $detail['quantity'],
+                $detail['price'],
+            );
+        }
+
+        $printer->calculateGrandTotal();
+
+        // Print receipt
+        $printer->printReceipt();
+    }
+}
